9f721cd7e8458b054d24474abfebe56e3ac24fed
lwh_holiday <lwh_holiday@outlook.com>
2026-02-25 11:18:53 +0800

    上传代码部署

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const { getFund } = require("./api/fund");
 const { getHistory } = require("./api/history");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
